3db5e9da64322acad31f7ce74d2f4231ca6744a2
Usama Amjid <usamaamjid7@gmail.com>
2023-08-12 19:15:02 +0500

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformaton
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -59,4 +60,6 @@ if __name__=='__main__':
     train_data,test_data=obj.inititate_data_ingestion()
 
     data_transformation=DataTransformaton()
-    data_transformation.inititate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.inititate_data_transformation(train_data,test_data)
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
